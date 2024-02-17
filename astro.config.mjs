e5071c92163dc00ff62a137060207667aa8340cc
Javier <vichango@gmail.com>
2024-02-18 00:49:19 +0100

    Connecting to Tina

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://vichango.github.io',
+  base: '/backbeat-astro',
   i18n: {
     defaultLocale: 'fr',
     locales: ['fr', 'en'],
