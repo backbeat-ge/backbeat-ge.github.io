8589cfdeba920c22e364920fd28243edf77da295
Javier <vichango@gmail.com>
2024-02-18 22:45:12 +0100

    Start creating content.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,15 +7,25 @@ export const defaultLang = "fr";
 
 export const ui = {
   fr: {
-    "alt.logo": "Notre logo",
+    "alt.logo": "Logo de notre association",
+    "nav.keyword.dance": "Danse",
+    "nav.keyword.music": "Musique",
+    "nav.keyword.community": "Communauté",
+    "nav.keyword.culture": "Culture Swing à Genève",
     "nav.home": "Accueil",
     "nav.events": "Événements",
     "nav.association": "L'association",
     "nav.charte": "Notre charte",
   },
   en: {
-    "alt.logo": "Our logo",
+    "alt.logo": "Our association's logo",
+    "nav.keyword.dance": "Dance",
+    "nav.keyword.music": "Music",
+    "nav.keyword.community": "Community",
+    "nav.keyword.culture": "Swing Culture in Geneva",
     "nav.home": "Home",
     "nav.events": "Events",
+    "nav.association": "The association",
+    "nav.charte": "Our chart",
   },
 } as const;
